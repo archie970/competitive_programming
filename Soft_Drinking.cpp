64cc93dba2334ecbab7c4a1cbbcc3bfcee8dcc4b
Saksham-13 <sakshamalok13@gmail.com>
2022-04-04 22:52:32 +0530

    added two files

---
+++ Soft_Drinking.cpp
@@ -0,0 +1,49 @@
+
+#include <iostream>
+
+
+
+using namespace std;
+bool comp(int a, int b)   
+{   
+    return (a < b);   
+}   
+int main()
+{   int ans =5;
+    // n, k, l, c, d, p, nl, np
+    int n, k, l, c, d, p, nl, np;
+    cin>>n>>k>>l>>c>>d>>p>>nl>>np;
+    int drinks = ((k*l)/nl)/n;
+    int lime = (c*d)/n;
+    int salt = (p/np)/n;
+    if (salt<lime)
+    {
+        /* i */
+        ans = salt;
+    }
+    else 
+    {
+        ans = lime;
+    }
+    if (ans>drinks)
+    {
+        ans=drinks;
+    }
+
+    cout<<ans;
+
+    
+        
+
+        
+
+
+
+
+    
+
+    
+
+
+	return 0;
+}
